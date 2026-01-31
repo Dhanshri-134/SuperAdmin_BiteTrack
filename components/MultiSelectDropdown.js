013760ef510564156a68b4c08cc6e6fd2c75d121
Dhanshri-134 <yewankardhanshri@gmail.com>
2026-01-31 16:48:34 +0530

    Improved UI

--- components/MultiSelectDropdown.js
+++ components/MultiSelectDropdown.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect, useMemo } from "react";
-import styles from "./MultiSelectDropdown.module.css";
+import styles from "./MultiselectDropdown.module.css";
 
 export default function MultiSelectDropdown({
   options = [],              // [{ label, value }]
